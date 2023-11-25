cfd922fbbc9d0b4af9a99b62a56be0f070e1a2a9
HafidhRizky <hafidhrzk@gmail.com>
2023-11-25 18:35:09 +0700

    new commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "token"
 
 @app.route("/")
 def base():
